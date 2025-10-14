d278b671f7678481fa1346e9a0a8d48012f853c9
Jenyve Riski Restian <jenyveriskirestian@gmail.com>
2025-10-14 14:58:55 +0700

    edit

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/halo', function () {
     return 'Halo, Laravel';
     });
+
+Route::get('/products', [ProductController::class, 'index']);
